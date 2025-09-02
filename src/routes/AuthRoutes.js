b908d5b7b73efcdd6e70b1c96a51768a90fc2c13
Sachin Raj <sachinraj6392@gmail.com>
2025-09-02 11:09:38 +0530

    add user and admin route

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -4,10 +4,21 @@ const router = express.Router();
 const userLogin = require('../controllers/LoginController');
 const userRegistration = require('../controllers/UserRegistration');
 
+const authenticate = require('../middlewares/authMiddleware');
+const authorizeRoles = require('../middlewares/roleMiddleware');
+
 // POST /auth/user/login
 router.post('/user/login', userLogin);
 
 // POST /auth/user/register
 router.post('/user/register', userRegistration);
+
+router.get('/user', authenticate, authorizeRoles('user', 'admin'), (req, res) => {
+    res.json({ message: 'Hello user!' });
+});
+
+router.get('/admin', authenticate, authorizeRoles('admin'), (req, res) => {
+    res.json({ message: 'Hello Admin!' });
+});
 
 module.exports = router;
