859aa62c634e75c8f8964bafa9fd509904b4f8e2
Sachin Raj <sachinraj6392@gmail.com>
2025-11-19 23:58:34 +0530

    make connect to menu and category table and written quary to get data

---
+++ src/models/menuModel.js
@@ -0,0 +1,20 @@
+const pool = require('../config/db');
+
+const getMenu = async () => {
+    const [rows] = await pool.query(`
+        SELECT 
+            m.id, 
+            m.name, 
+            m.descriptions,
+            m.image_urls,
+            m.price,
+            m.discount,
+            c.c_name,
+            m.is_active
+        FROM menu m 
+        LEFT JOIN categories c ON m.category_id = c.id
+    `);
+    return rows;
+};
+
+module.exports = getMenu;
