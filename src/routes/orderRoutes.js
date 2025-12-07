72dbc8532309c93ca92a4740cef010a20d7740b6
Sachin Raj <sachinraj6392@gmail.com>
2025-12-07 17:29:55 +0530

    add order Status

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { placeOrder } = require("../controllers/orderController");
+const OrderStatus = require("../controllers/OrderStatusController");   // ✅ ADD THIS LINE
 
 router.post("/orders", placeOrder);
+router.get("/status/:invoice_id", OrderStatus);  // ✅ use GET
 
 module.exports = router;
