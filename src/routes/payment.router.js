d6acd37e048618ab0dbad808752524b9210aa0ee
Sachin Raj <sachinraj6392@gmail.com>
2025-12-29 01:40:43 +0530

    add payment router

--- src/routes/payment.router.js
+++ src/routes/payment.router.js
@@ -4,9 +4,11 @@ const router = express.Router();
 // Controllers
 const createOrder = require("../controllers/Payment/PaymentController");
 const verifyPayment = require("../controllers/Payment/VerifyPaymentController");
+const paymentFailed = require("../controllers/Payment/UpdatePaymentFail");
 
 // Routes
-router.post("/create-order", createOrder);
-router.post("/verify-payment", verifyPayment);
+router.post("/create-order", createOrder);      
+router.post("/verify-payment", verifyPayment);  
+router.post("/payment-failed", paymentFailed);  
 
 module.exports = router;
